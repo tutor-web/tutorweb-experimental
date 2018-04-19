6cf80474a079a1c93392e86ac1560c55ed23020a
Jamie Lentin <jm@lentin.co.uk>
2018-04-19 16:05:36 +0100

    server: Remove dead code from subscriptions.list

--- server/tutorweb_quizdb/subscriptions/list.py
+++ server/tutorweb_quizdb/subscriptions/list.py
@@ -1,55 +1,8 @@
 import os
 import urllib.parse
 
-import rpy2.robjects as robjects
-
 from tutorweb_quizdb import DBSession, Base
 from tutorweb_quizdb.student import get_current_student
-
-
-MATERIAL_BANK = os.path.normpath(os.path.join(os.path.dirname(__file__), '../../../db/material_bank'))  # TODO: This should be configured centrally, somewhere.
-
-
-def rlist_to_dict(a):
-    """
-    Take R ListVector, turn it into a dict
-    """
-    return dict(zip(a.names, map(list, list(a))))
-
-
-def render(path, permutation):
-    """
-    Render a question
-    """
-    # Is this permutation even a question?
-    ms = DBSession.query(Base.classes.materialsource).filter_by(path=path, nextrevision=None).one()
-    if ms.permutationcount > permutation:
-        raise ValueError("Question %s only has %d permutations, not %d" % (
-            path,
-            ms.permutationcount,
-            permutation,
-        ))
-
-    # TODO: Caching of question objects?
-    robjects.r('''question <- function () stop("R question script did not define a question function")''')
-    robjects.r('''setwd''')(os.path.dirname(os.path.join(MATERIAL_BANK, path)))
-    robjects.r('''source''')(os.path.basename(path))
-    # TODO: data.frames support
-
-    rob = robjects.globalenv['question'](permutation, [])
-    # TODO: Stacktraces?
-
-    rv = {}
-    for i, name in enumerate(rob.names):
-        if name == 'content':
-            rv[name] = "".join(rob[i])
-        elif name == 'correct':
-            rv[name] = rlist_to_dict(rob[i])
-        else:
-            raise ValueError("Unknown return value from R question %s - %s" % (path, name))
-    if 'content' not in rv:
-        raise ValueError("R question %s did not return 'content'" % path)
-    return rv
 
 
 def view_subscription_list(request):
