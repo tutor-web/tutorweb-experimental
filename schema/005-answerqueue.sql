a80feb7c7d32f042bd50eb0356f0ba322ca72e8d
Jamie Lentin <jm@lentin.co.uk>
2018-05-25 10:37:15 +0100

    schema: answer_stats / stage_material views to select questions from

--- schema/005-answerqueue.sql
+++ schema/005-answerqueue.sql
@@ -24,4 +24,31 @@ CREATE TABLE IF NOT EXISTS answer (
 );
 
 
+CREATE OR REPLACE VIEW answer_stats AS
+    SELECT a.stage_id
+         , a.material_source_id
+         , COUNT(*) chosen
+         , COUNT(a.correct) correct
+    FROM answer a
+    GROUP BY 1, 2;
+COMMENT ON VIEW answer_stats IS 'Answer stats for each stage/material_source combo';
+
+
+CREATE OR REPLACE VIEW stage_material AS
+    SELECT s.stage_id
+         , ms.material_source_id
+         , GENERATE_SERIES(1, ms.permutation_count) "permutation"
+         , COALESCE(stats.chosen, 0) chosen
+         , COALESCE(stats.correct, 0) correct
+    FROM stage s
+    JOIN material_source ms
+      ON s.material_tags <@ ms.material_tags
+    LEFT JOIN answer_stats stats
+           ON ms.material_source_id = stats.material_source_id
+          AND s.stage_id = stats.stage_id
+    WHERE ms.next_revision IS NULL
+      AND s.next_version IS NULL;
+COMMENT ON VIEW stage_material IS 'All appropriate material for all stages, and their stats';
+
+
 COMMIT;
