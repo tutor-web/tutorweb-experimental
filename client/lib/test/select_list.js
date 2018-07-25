563653043392309afbc4e5081fe59f5c817a0bfd
Jamie Lentin <jm@lentin.co.uk>
2018-07-25 11:35:04 +0100

    client/select_list: Add test framework for select_list

---
+++ client/lib/test/select_list.js
@@ -0,0 +1,27 @@
+"use strict";
+/*jslint nomen: true, plusplus: true, browser:true, todo: true */
+/*global require */
+
+var h = require('hyperscript');
+var jQuery = require('jquery');
+var select_list = require('lib/select_list.js').select_list;
+
+function renderLink (data) {
+    return h('a', { href: '#' }, [data.title]);
+}
+
+jQuery(document.getElementById('tw-quiz')).append([
+    select_list([
+        {title: "Cows", children: [
+            {title: "Daisy", children: [
+                {title: "Legs: 4"},
+                {title: "Noise: moo"},
+            ]},
+            {title: "Freda"},
+        ]},
+        {title: "Pigs", children: [
+            {title: "George"},
+            {title: "Frank"},
+        ]},
+    ], renderLink)
+]);
