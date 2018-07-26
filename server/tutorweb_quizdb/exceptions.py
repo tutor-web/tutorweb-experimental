77568e911b7534df823b1033e9474b7a984c1c9c
Jamie Lentin <jm@lentin.co.uk>
2018-07-26 11:00:00 +0100

    server/exceptions: Send back correct respose for HTTPException/HTTPBadRequest

--- server/tutorweb_quizdb/exceptions.py
+++ server/tutorweb_quizdb/exceptions.py
@@ -1,9 +1,14 @@
+from pyramid.httpexceptions import (
+    HTTPException,
+    HTTPBadRequest
+)
+
 def view_exception(e, request):
     import traceback
 
     level = getattr(e, 'level', 'error')
     print_stack = getattr(e, 'print_stack', True)
-    request.response.status = getattr(e, 'http_status', 500)
+    request.response.status = getattr(e, 'status_code', 500)
     return {level: dict(
         message=(e.__class__.__name__ + ": " if print_stack else "") + str(e),
         stack=traceback.format_exc() if print_stack else None,
@@ -12,3 +17,5 @@ def view_exception(e, request):
 
 def includeme(config):
     config.add_exception_view(view_exception, Exception, renderer='json')
+    config.add_exception_view(view_exception, HTTPException, renderer='json')
+    config.add_exception_view(view_exception, HTTPBadRequest, renderer='json')
