69106db318e51b909798a3fa1187264a92871b96
Jamie Lentin <jm@lentin.co.uk>
2019-04-30 17:07:19 +0100

    client/preview: Form to select question path/permutation

--- client/lib/preview.js
+++ client/lib/preview.js
@@ -1,6 +1,7 @@
 "use strict";
 /*jslint todo: true, regexp: true, browser: true, unparam: true */
 /*global Promise */
+var formson = require('formson');
 var jQuery = require('jquery');
 var View = require('lib/view.js');
 var renderTex = require('lib/rendertex.js').renderTex;
@@ -12,12 +13,24 @@ function page_load(e) {
         ajaxApi = new AjaxApi(jQuery.ajax),
         qs = parse_qs(window.location);
 
-    return ajaxApi.getJson('/api/material/render?path=' + encodeURIComponent(qs.path) + '&permutation=' + encodeURIComponent(qs.permutation)).then(function (material) {
-        console.log(material);
+    formson.update_form(document.forms.preview_select, qs);
+    if (!qs.path) {
+        return;
+    }
+
+    return ajaxApi.getJson('/api/material/render?path=' + encodeURIComponent(qs.path) + '&permutation=' + encodeURIComponent(qs.permutation || 1)).then(function (material) {
         twView.jqQuiz.html(material.content);
+
+        // Replace placeholders with the real content
+        Array.prototype.map.call(twView.jqQuiz[0].querySelectorAll('div.reveal-on-answer-placeholder'), function (el) {
+            el.parentNode.replaceChild(jQuery(el).data('orig'), el);
+        });
+
         twView.renderMath();
     }).then(function () {
         twView.jqQuiz.removeClass('busy');
+    }).catch(function (err) {
+        twView.showAlert('error', err.message);
     });
 }
 
