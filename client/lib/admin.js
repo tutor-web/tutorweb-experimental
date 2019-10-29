fcca849ce7ede19da386236d8930de63c8946aa3
Jamie Lentin <jm@lentin.co.uk>
2019-10-29 18:08:07 +0000

    client/admin: Add admin page for viewing student results

---
+++ client/lib/admin.js
@@ -0,0 +1,44 @@
+"use strict";
+/*jslint todo: true, regexp: true, browser: true, unparam: true */
+/*global Promise */
+var parse_qs = require('lib/parse_qs.js').parse_qs;
+var View = require('lib/view.js');
+var Handsontable = require('handsontable');
+var jQuery = require('jquery');
+var AjaxApi = require('lib/ajaxapi.js');
+
+function page_load(qs, state) {
+    var twView = new View(jQuery),
+        ajaxApi = new AjaxApi(jQuery.ajax);
+
+    Array.prototype.forEach.call(document.querySelectorAll('#csv-links a'), function (a_el) {
+        a_el.href = a_el.href + '?path=' + qs.path;
+    });
+
+    return ajaxApi.getJson('/api/syllabus/student_grades?path=' + qs.path).then(function (data) {
+        var headers, hot, hot_el = document.getElementById('results-hot');
+
+        headers = data.results.shift();
+        hot = new Handsontable(hot_el, {
+            stretchH: 'all',
+            autoWrapRow: true,
+            colHeaders: headers,
+            data: data.results,
+        });
+        window.hot = hot;
+    }).catch(function (err) {
+        if (err.message.indexOf('tutorweb::unauth::') === 0) {
+            // TODO: "Not an admin" caught by this too
+            // i.e. do go-login
+            window.location.href = '/auth/login?next=' + encodeURIComponent(window.location.pathname + window.location.search);
+        }
+        console.error(err);
+        twView.showAlert('error', err.message);
+    });
+}
+
+if (window) {
+    document.addEventListener('DOMContentLoaded', function (e) {
+        page_load(parse_qs(window.location), window.history.state || {});
+    });
+}
