01cdeec6bf7e81a69a3f1fc79a34633b1bc347f3
Jamie Lentin <jm@lentin.co.uk>
2020-05-29 18:09:55 +0100

    server/auth/policy: Request JSONSerializer to quell warnings

--- server/tutorweb_quizdb/auth/policy.py
+++ server/tutorweb_quizdb/auth/policy.py
@@ -1,6 +1,6 @@
 from pyramid.authentication import AuthTktAuthenticationPolicy
 from pyramid.authorization import ACLAuthorizationPolicy
-from pyramid.session import SignedCookieSessionFactory
+from pyramid.session import SignedCookieSessionFactory, JSONSerializer
 
 from tutorweb_quizdb import DBSession, models
 
@@ -25,5 +25,5 @@ def includeme(config):
     machine_id = read_machine_id()
     config.set_authorization_policy(ACLAuthorizationPolicy())
     config.set_authentication_policy(AuthTktAuthenticationPolicy(machine_id + '-auth'))
-    config.set_session_factory(SignedCookieSessionFactory(machine_id + '-session'))
+    config.set_session_factory(SignedCookieSessionFactory(machine_id + '-session', serializer=JSONSerializer))
     config.add_request_method(get_user, 'user', reify=True)  # populate request.user on request
